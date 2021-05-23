d34f0bd768cff2264fb2979a2ae3f49571823c61
bilal1031 <bilalnaeem166@gmail.com>
2021-05-23 23:42:00 +0500

    added

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import React, { useEffect, useState } from "react";
 import { LogBox, Text, View, StyleSheet } from "react-native";
 import firebase from "firebase";
 import AppLoading from "expo-app-loading";
-import LoginScreen from "./src/screens/LoginScreen";
+import MainScreen from "./src/screen/MainScreen";
 
-const firebaseConfig = () => {
-  firebase.initializeApp({
-    apiKey: "AIzaSyDqNkSJMtZmaE4F_cA9u1_ZOwZJGtpFkoY",
-    authDomain: "the-review-book.firebaseapp.com",
-    projectId: "the-review-book",
-    storageBucket: "the-review-book.appspot.com",
-    messagingSenderId: "475844666441",
-    appId: "1:475844666441:web:bcb6d27e3a098fabb48b98",
-    measurementId: "G-XZ1J7062DV",
-  });
-};
+// const firebaseConfig = () => {
+//   firebase.initializeApp({
+//     apiKey: "AIzaSyDqNkSJMtZmaE4F_cA9u1_ZOwZJGtpFkoY",
+//     authDomain: "the-review-book.firebaseapp.com",
+//     projectId: "the-review-book",
+//     storageBucket: "the-review-book.appspot.com",
+//     messagingSenderId: "475844666441",
+//     appId: "1:475844666441:web:bcb6d27e3a098fabb48b98",
+//     measurementId: "G-XZ1J7062DV",
+//   });
+// };
 
 /*
 // we can't use useAuthentication's login, because useAuthentication use useUser
@@ -30,41 +31,53 @@ const _logIn = (email, password) => {
     });
 };
 */
-
+const Stack = createStackNavigator();
 export default function App() {
-  const [isReady, setIsReady] = useState(false);
-  LogBox.ignoreLogs([""]);
+  ///asdasdasd
+  //   const [isReady, setIsReady] = useState(false);
+  //   LogBox.ignoreLogs([""]);
 
-  useEffect(() => {
-    if (!firebase.app.length) firebaseConfig();
-  }, []);
+  //   useEffect(() => {
+  //     if (!firebase.app.length) firebaseConfig();
+  //   }, []);
 
-  const authUser = async () => {
-    firebaseConfig();
-    /*
-    const result = await secureStorage.readUser();
-    if (!result) return;
-    const parsed = JSON.parse(result);
+  //   const authUser = async () => {
+  //     firebaseConfig();
+  //     /*
+  //     const result = await secureStorage.readUser();
+  //     if (!result) return;
+  //     const parsed = JSON.parse(result);
 
-    setUser(parsed);
-    _logIn(parsed.email, parsed.password, setUser);
-*/
-  };
+  //     setUser(parsed);
+  //     _logIn(parsed.email, parsed.password, setUser);
+  // */
+  //   };
 
-  if (!isReady) {
-    return (
-      <AppLoading
-        startAsync={authUser}
-        onFinish={() => setIsReady(true)}
-        onError={console.log("")}
-      />
-    );
-  }
+  // if (!isReady) {
+  //   return (
+  //     <AppLoading
+  //       startAsync={authUser}
+  //       onFinish={() => setIsReady(true)}
+  //       onError={console.log("")}
+  //     />
+  //   );
+  // }
 
   return (
+<<<<<<< HEAD
     <View style={styles.container}>
       <LoginScreen />
     </View>
+=======
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="mainscreen"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="mainscreen" component={MainScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+>>>>>>> bilal
   );
 }
 
