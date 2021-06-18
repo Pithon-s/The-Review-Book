3550d84f39187f751f4247d8f9a2573c69cc7f44
Ibrahim-Arif <ibrahimrana232@gmail.com>
2021-06-18 23:16:58 +0500

    warning handled

--- App.js
+++ App.js
@@ -56,7 +56,13 @@ const NavigationImp = () => {
 
 export default function App() {
   const store = createStore(Reducers, applyMiddleware(thunk));
-  LogBox.ignoreAllLogs();
+  LogBox.ignoreLogs([
+    "Setting a timer for a long period of time",
+    "VirtualizedLists should never be nested inside",
+    'Each child in a list should have a unique "key" prop',
+    "undefined is not a function (near '...}).catch(function (error) {...')",
+    "Function Query.where() called with invalid data",
+  ]);
 
   return (
     <Provider store={store}>
